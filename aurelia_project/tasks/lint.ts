f4534aaa8321b50754ab67fd76fe2b96a2c57ff6
Vildan Softic <zewa666@gmail.com>
2019-12-14 08:59:11 +0100

    feat(app): added stylelint with custom rulesets

--- aurelia_project/tasks/lint.ts
+++ aurelia_project/tasks/lint.ts
@@ -1,12 +1,23 @@
 import * as gulp from 'gulp';
 import tslint from 'gulp-tslint';
+const gulpStylelint = require('gulp-stylelint');
+const merge = require("merge-stream");
 import * as project from '../aurelia.json';
 
 export default function lint() {
-  return gulp.src(project.transpiler.source)
+  const tsLinting = gulp.src(project.transpiler.source)
     .pipe(tslint({
       tslint: require("tslint"),
       formatter: 'prose'
     }))
     .pipe(tslint.report());
+
+  const scssLinting = gulp.src(project.cssProcessor.source)
+    .pipe(gulpStylelint({
+      reporters: [
+        { formatter: 'string', console: true }
+      ]
+    }));
+
+  return merge(tsLinting, scssLinting);
 }
