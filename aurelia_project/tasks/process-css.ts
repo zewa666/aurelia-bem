fc6011c0e74d7d3539bb2dae1f3a1b92e6783bfb
Vildan Softic <zewa666@gmail.com>
2019-12-14 18:52:51 +0100

    chore(app): switch to gulp-dart-sass for module support

--- aurelia_project/tasks/process-css.ts
+++ aurelia_project/tasks/process-css.ts
@@ -1,10 +1,12 @@
-import {build} from 'aurelia-cli';
+import { build } from 'aurelia-cli';
 import * as gulp from 'gulp';
 import * as project from '../aurelia.json';
-import * as sass from 'gulp-sass';
+import * as sass from 'gulp-dart-sass';
+
+const sourcemaps = require('gulp-sourcemaps');
 
 export default function processCSS() {
-  return gulp.src(project.cssProcessor.source, {sourcemaps: true})
+  return gulp.src(project.cssProcessor.source, { sourcemaps: true })
     .pipe(sass().on('error', sass.logError))
     .pipe(build.bundle());
 }
@@ -12,7 +14,9 @@ export default function processCSS() {
 export function pluginCSS(dest) {
   return function processPluginCSS() {
     return gulp.src(project.plugin.source.css)
+      .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
+      .pipe(sourcemaps.write())
       .pipe(gulp.dest(dest));
   };
 }
